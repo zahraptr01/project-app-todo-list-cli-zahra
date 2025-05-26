bd9a5ddbb658233f14c5feaa75ee0d0900fc9c2b
zahraptr01 <zahrazahraputri57@gmail.com>
2025-05-26 17:53:00 +0700

    feat: add Data Structures and Operations

--- task/task.go
+++ task/task.go
@@ -1,1 +1,30 @@
 package task
+
+import (
+	"fmt"
+	"strings"
+)
+
+type Task struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Completed   bool   `json:"completed"`
+}
+
+func (t Task) String() string {
+	status := "✗"
+	if t.Completed {
+		status = "✓"
+	}
+	return fmt.Sprintf("%s\t%s\t%s", status, t.Title, t.Description)
+}
+
+// function to check duplication of task title data
+func IsDuplicate(tasks []Task, title string) bool {
+	for _, t := range tasks {
+		if strings.EqualFold(t.Title, title) {
+			return true
+		}
+	}
+	return false
+}
