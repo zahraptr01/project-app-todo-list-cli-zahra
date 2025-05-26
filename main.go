1e75c12f408c5fed17f08b2ae58a7d25b3624b51
zahraptr01 <zahrazahraputri57@gmail.com>
2025-05-26 19:02:20 +0700

    feat: add CLI and Logic Program

--- main.go
+++ main.go
@@ -1,1 +1,48 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"project-app-todo-list-cli-zahra/task"
+)
+
+// function for command-line flag declaration
+func main() {
+	add := flag.String("add", "", "Tambah tugas baru")
+	desc := flag.String("desc", "", "Deskripsi tugas")
+	list := flag.Bool("list", false, "Tampilkan semua tugas")
+	done := flag.String("done", "", "Tandai tugas sebagai selesai")
+	deleteTask := flag.String("delete", "", "Hapus tugas")
+	search := flag.String("search", "", "Cari tugas")
+	flag.Parse()
+
+	// Read task data from file
+	tasks, err := task.LoadTasks()
+	// Error Handling
+	if err != nil {
+		fmt.Println("Gagal memuat tugas:", err)
+		os.Exit(1)
+	}
+
+	// Switch to handle each operation
+	if *add != "" {
+		err = task.AddTask(&tasks, *add, *desc)
+	} else if *list {
+		task.PrintTasks(tasks)
+	} else if *done != "" {
+		err = task.MarkDone(&tasks, *done)
+	} else if *deleteTask != "" {
+		err = task.DeleteTask(&tasks, *deleteTask)
+	} else if *search != "" {
+		task.SearchTasks(tasks, *search)
+	} else {
+		fmt.Println("Gunakan --help untuk melihat opsi.")
+	}
+
+	// Error Handling
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+}
